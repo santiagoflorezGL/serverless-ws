66746c8a51799cc70f5955350a77c77b38094a7e
Santiago Flórez Giraldo <santiago.florez@Santiago-Florez-MacBook-Pro.local>
2021-12-07 10:23:26 -0500

    Sanity setup and functions

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,9 @@
 exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || '';
   console.log("Hello, you!");
 
   return {
     statusCode: 200,
-    body: "it twerks",
+    body: "it twerks " + name,
   };
 };
