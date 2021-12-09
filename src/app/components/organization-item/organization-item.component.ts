d3497f8404fe8f211ed129a5c58391cd9179b9b5
Santiago Flórez Giraldo <santiago.florez@Santiago-Florez-MacBook-Pro.local>
2021-12-09 12:32:09 -0500

    Set stripe

--- src/app/components/organization-item/organization-item.component.ts
+++ src/app/components/organization-item/organization-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
+import { lastValueFrom } from 'rxjs';
+
 import { Organization } from 'src/app/models/organization.model';
 
 @Component({
@@ -17,9 +21,29 @@ export class OrganizationItemComponent implements OnInit {
     image: 'https://bit.ly/2ZdePYO',
   };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  async triggerCreateCheckout(eventOrganization: any) {
+    const createCheckoutResponse = await this.http.post(
+      "/.netlify/functions/createCheckout",
+      eventOrganization,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const lastResponse = await lastValueFrom(createCheckoutResponse);
+    this.openStripe(lastResponse);
+  }
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    })
+  }
 }
